dcde84b5d11dd4036a52d33afa28052adf3e7768
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-04-28 01:30:13 +0800

    Pseudocode

---
+++ Number_Extractor.py
@@ -0,0 +1,15 @@
+# Ezrhael R. Sicat
+# BSCpE 1-5
+# 04/27/2023
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+# The program will create two other text file; the first text file will be named even.txt 
+# that will contains all even numbers extracted from the numbers.txt. The second text file 
+# will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+# ask user for input
+# write the numbers to the file
+# read the contents of the file
+# loop through each number and add it to the appropriate list
+# convert the list into string
+# display output
+# write odd and even numbers to files
